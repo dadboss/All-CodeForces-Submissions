7f8699b384b2f5be3be0f50886fbd32d2dbffceb
dadboss <shivamjha1206@gmail.com>
2019-07-26 01:55:00 +0530

    Add solution for problem `B - Two Buttons` Link: https://codeforces.com/contest/520/problem/B Tags: dfs and similar, graphs, greedy, implementation, math, shortest paths, *1400 Ref: https://codeforces.com/contest/520/submission/57765923

---
+++ codeforces/520/B.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     long long int n,c=0,m;
+    cin>>n>>m;
+    while(m>n)
+    {
+        if(m%2)
+            m++;
+        else m/=2;
+        c++;
+    }
+    cout<<c+n-m;
+
+}
+
