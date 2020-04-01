6e1bf1503d92a2898902e965921e82617c416823
dadboss <shivamjha1206@gmail.com>
2020-04-01 20:54:00 +0530

    Add solution for problem `D - Again?` Link: https://codeforces.com/contest/1331/problem/D Tags: *special problem, implementation Ref: https://codeforces.com/contest/1331/submission/75133824

---
+++ codeforces/1331/D.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define ff first
+#define ss second
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n";
+#define OK cout<<"OK";
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    string s;
+    cin>>s;
+    if((s[s.size()-1]-'0')%2)
+        cout<<1;
+    else
+        cout<<0;
+}
