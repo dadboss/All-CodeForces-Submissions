927a07bd74260d731cd054b5985e8ab9b6e8aca7
dadboss <shivamjha1206@gmail.com>
2020-05-06 10:30:00 +0530

    Add solution for problem `B - New Year's Eve` Link: https://codeforces.com/contest/912/problem/B Tags: bitmasks, constructive algorithms, number theory, *1300 Ref: https://codeforces.com/contest/912/submission/79107559

---
+++ codeforces/912/B.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define mp makepair
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n" 
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define all(v) (v).begin(),(v).end()
+int main()
+{
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ // freopen("input.txt", "r", stdin);
+  //freopen("output.txt", "w", stdout);
+  ll n,k;
+  cin>>n>>k;
+  if(k==1)	
+  	deb(n)
+  else
+  {
+  	ll i=63;
+  	bitset<64> b(n);
+  	while(b[i]==0)
+  		i--;
+  	deb((1ll<<(i+1))-1);
+  }
+}
