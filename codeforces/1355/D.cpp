e0463d4c40a6d76abd4cced88bf1063507e2164e
dadboss <shivamjha1206@gmail.com>
2020-05-16 18:03:00 +0530

    Add solution for problem `D - Game With Array` Link: https://codeforces.com/contest/1355/problem/D Tags: constructive algorithms, math, *1400 Ref: https://codeforces.com/contest/1355/submission/80344337

---
+++ codeforces/1355/D.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define mp makepair
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n" 
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define all(v) (v).begin(),(v).end()
+int main()
+{
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  //freopen("input.txt", "r", stdin);
+  //freopen("output.txt", "w", stdout);
+  ll n,s;
+  cin>>n>>s;
+  if(s<2*n)
+  {
+  	NO
+  	return 0;
+  }
+  YES
+  ll i=1;
+  while(i<n)
+  {
+  	cout<<"1 ";
+  	i++;
+  }
+  deb(s-n+1)
+  deb(s-n)
+}
