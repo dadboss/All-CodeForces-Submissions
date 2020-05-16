42a22f7dcc57d3a51cf86e2998358bd55021b241
dadboss <shivamjha1206@gmail.com>
2020-05-16 17:13:00 +0530

    Add solution for problem `A - Sequence with Digits` Link: https://codeforces.com/contest/1355/problem/A Tags: brute force, implementation, math, *1200 Ref: https://codeforces.com/contest/1355/submission/80311855

---
+++ codeforces/1355/A.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define mp makepair
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n" 
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define all(v) (v).begin(),(v).end()
+int main()
+{
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  //freopen("input.txt", "r", stdin);
+  //freopen("output.txt", "w", stdout);
+  ll t,a,mn,mx,b,k;
+  cin>>t;
+  while(t--)
+  {
+  	cin>>a>>k;
+  	k--;
+  	while(k--)
+  	{
+  		b=a;
+  		mn=9;
+  		mx=0;
+  		while(b)
+  		{
+  			mn=min(b%10,mn);
+  			mx=max(b%10,mx);
+  			b/=10;
+  		}
+  		a+=mn*mx;
+  		if(mn==0)
+  			break;
+  	}
+  	deb(a);
+  }
+}
