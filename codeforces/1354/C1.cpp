5e5161afbe2d2da80010fbafc000ba430b1bda12
dadboss <shivamjha1206@gmail.com>
2020-05-17 16:03:00 +0530

    Add solution for problem `C1 - Simple Polygon Embedding` Link: https://codeforces.com/contest/1354/problem/C1 Tags: binary search, geometry, math, ternary search, *1400 Ref: https://codeforces.com/contest/1354/submission/80493530

---
+++ codeforces/1354/C1.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define mp makepair
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n" 
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define all(v) (v).begin(),(v).end()
+#define p 3.141592653589793238
+int main()
+{
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  //freopen("input.txt", "r", stdin);
+  //freopen("output.txt", "w", stdout);
+  ll t,n;
+  cin>>t;
+  while(t--)
+  {
+  	cin>>n;
+  	n+=n;
+  	double x=(n-2.0)*p/(n*2.0);
+  	//deb(x)
+  	cout<<fixed<<setprecision(10)<<tan(x)<<"\n";
+  }
+}
