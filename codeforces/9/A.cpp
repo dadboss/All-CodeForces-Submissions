039dc930eb978f5426e6deb3e7de62095afa3a61
dadboss <shivamjha1206@gmail.com>
2020-05-26 18:39:00 +0530

    Add solution for problem `A - Die Roll` Link: https://codeforces.com/contest/9/problem/A Tags: math, probabilities, *800 Ref: https://codeforces.com/contest/9/submission/81473235

---
+++ codeforces/9/A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define mp makepair
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n" 
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define all(v) (v).begin(),(v).end()
+int main()
+{
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  //freopen("input.txt", "r", stdin);
+  //freopen("output.txt", "w", stdout);
+  ll a,b;
+  cin>>a>>b;
+  a=max(a,b);
+  if(a>6)
+  {
+  	deb("0/1")
+  }
+  else
+  	{
+  		b=6-a+1;
+  		//deb(b)
+  		cout<<b/__gcd(6ll,b)<<'/'<<6/__gcd(6ll,b);
+  	}
+}
