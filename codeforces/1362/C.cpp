0ea67cc3d6f1b87adcaabb2a66f28c1198b09d3c
dadboss <shivamjha1206@gmail.com>
2020-06-04 20:44:00 +0530

    Add solution for problem `C - Johnny and Another Rating Drop` Link: https://codeforces.com/contest/1362/problem/C Tags: bitmasks, greedy, math, *1400 Ref: https://codeforces.com/contest/1362/submission/82516642

---
+++ codeforces/1362/C.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define mp makepair
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n" 
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define all(v) (v).begin(),(v).end()
+int main()
+{
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  //freopen("input.txt", "r", stdin);
+  //freopen("output.txt", "w", stdout);
+  ll t,n;
+  cin>>t;
+  while(t--)
+  {
+    cin>>n;
+    deb(2*n-__builtin_popcountll(n))
+  }
+}
