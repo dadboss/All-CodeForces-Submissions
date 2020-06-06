409e0cbfe4a6f1f390e26ed9878e6c398b5a7f79
dadboss <shivamjha1206@gmail.com>
2020-06-07 01:45:00 +0530

    Add solution for problem `C - p-binary` Link: https://codeforces.com/contest/1225/problem/C Tags: bitmasks, brute force, math, *1600 Ref: https://codeforces.com/contest/1225/submission/82735904

---
+++ codeforces/1225/C.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define mp makepair
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n" 
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define all(v) (v).begin(),(v).end()
+int main()
+{
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  //freopen("input.txt", "r", stdin);
+  //freopen("output.txt", "w", stdout);
+  ll n,p;
+  cin>>n>>p;
+  ll i=0;
+  while(i<70)
+  {
+    if(__builtin_popcount(n-i*p)<=i&&n>=i*p&&n-i*p>=i)
+    {
+      deb(i)
+      return 0;
+    }
+    i++;
+  }
+  deb(-1)
+}
