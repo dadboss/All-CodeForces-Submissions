5c832e3c057388bffafaf2cd12e36f97c42d6746
dadboss <shivamjha1206@gmail.com>
2020-06-10 16:48:00 +0530

    Add solution for problem `D1 - Add on a Tree` Link: https://codeforces.com/contest/1189/problem/D1 Tags: trees, *1600 Ref: https://codeforces.com/contest/1189/submission/83293949

---
+++ codeforces/1189/D1.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define mp makepair
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n" 
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define all(v) (v).begin(),(v).end()
+vector<ll> a[100009];
+int main()
+{
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  //freopen("input.txt", "r", stdin);
+  //freopen("output.txt", "w", stdout);
+  ll n;
+  cin>>n;
+  ll i=n-1,l,r;
+  while(i--)
+  {
+    cin>>l>>r;
+    a[l].pb(r);
+    a[r].pb(l);
+  }
+  i=1;
+  while(i<=n)
+  {
+    if(a[i].size()==2)
+    {
+      NO
+      return 0;
+    }
+    i++;
+  }
+  YES
+}
