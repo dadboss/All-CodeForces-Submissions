1bd1bc60f87ca4cbad85c077e21a0648e96c919f
dadboss <shivamjha1206@gmail.com>
2020-08-10 19:42:00 +0530

    Add solution for problem `B - Assigning to Classes` Link: https://codeforces.com/contest/1300/problem/B Tags: greedy, implementation, sortings, *1000 Ref: https://codeforces.com/contest/1300/submission/89516735

---
+++ codeforces/1300/B.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define mp makepair
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n" 
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define all(v) (v).begin(),(v).end()
+ll a[200009];
+int main()
+{
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  //freopen("input.txt", "r", stdin);
+  //freopen("output.txt", "w", stdout);
+  ll t,i,n;
+  cin>>t;
+  while(t--)
+  {
+    cin>>n;
+    i=0;
+    while(i<2*n)
+      cin>>a[i++];
+    sort(a,a+2*n);
+    deb(a[n]-a[n-1])
+  }
+}
