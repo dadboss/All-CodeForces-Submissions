45c66dff29b1e854e627338485d57bc7eea4f136
dadboss <shivamjha1206@gmail.com>
2020-08-20 17:30:00 +0530

    Add solution for problem `E - New Year Parties` Link: https://codeforces.com/contest/1283/problem/E Tags: dp, greedy, *1800 Ref: https://codeforces.com/contest/1283/submission/90460007

---
+++ codeforces/1283/E.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n";
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define clt(x) 63-__builtin_clzll(x)
+#define bct(x) __builtin_popcountll(x)
+#define all(v) (v).begin(),(v).end()
+#define pi pair<ll,ll>
+#define vi vector<ll>
+#define vpi vector<pair<ll,ll>>
+#define maxq priority_queue<ll>
+#define minq priority_queue<ll,vector<ll>, greater<ll>>
+#define cont continue;
+#define reto return 0;
+#define sz size()
+#define spmod 1116295198451
+#define mod 1000000007
+#define md 998244353 
+#define N 200009
+//typedef tree<int,null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> //pbds;
+
+ll a[N],f[N],g[N];
+int main()
+{
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  //freopen("input.txt", "r", stdin);
+  //freopen("output.txt", "w", stdout);
+  ll n;
+  cin>>n;
+  ll i=0;
+  while(i<n)
+    cin>>a[i++];
+  sort(a,a+n);
+  i=0;
+  ll ans=0,sum=0;
+  while(i<n)
+  {
+    if(f[a[i]]==0&&f[a[i]-1]==0&&f[a[i]+1]==0)
+    {
+      ans++;
+      f[a[i]+1]=1;
+    }
+    if(g[a[i]-1]==0)
+    {
+      sum++;
+      g[a[i]-1]=1;
+    }
+    else if(g[a[i]]==0)
+    {
+      sum++;
+      g[a[i]]=1;
+    }
+    else if(g[a[i]+1]==0)
+    {
+      sum++;
+      g[a[i]+1]=1;
+    }
+    i++;
+  }
+  deB(ans,sum)
+}
