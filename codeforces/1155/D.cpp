ca391515ffedbd8b54336cafb45448b00e34d597
dadboss <shivamjha1206@gmail.com>
2020-08-21 14:37:00 +0530

    Add solution for problem `D - Beautiful Array` Link: https://codeforces.com/contest/1155/problem/D Tags: brute force, data structures, divide and conquer, dp, greedy, *1900 Ref: https://codeforces.com/contest/1155/submission/90517862

---
+++ codeforces/1155/D.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n";
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define clt(x) 63-__builtin_clzll(x)
+#define bct(x) __builtin_popcountll(x)
+#define all(v) (v).begin(),(v).end()
+#define pi pair<ll,ll>
+#define vi vector<ll>
+#define vpi vector<pair<ll,ll>>
+#define maxq priority_queue<ll>
+#define minq priority_queue<ll,vector<ll>, greater<ll>>
+#define cont continue;
+#define reto return 0;
+#define sz size()
+#define spmod 1116295198451
+#define mod 1000000007
+#define md 998244353 
+#define N 300009
+//typedef tree<int,null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> //pbds;
+
+ll a[N],dp[N][3];
+int main()
+{
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  //freopen("input.txt", "r", stdin);
+  //freopen("output.txt", "w", stdout);
+  ll n,x,ans=0;
+  cin>>n>>x;
+  ll i=1;
+  while(i<=n)
+    cin>>a[i++];
+  i=1;
+  while(i<=n)
+  {
+    dp[i][0]=max({0ll,a[i],a[i]+dp[i-1][0]});
+    dp[i][1]=max({0ll,a[i],x*a[i],x*a[i]+dp[i-1][1],x*a[i]+dp[i-1][0]});
+    dp[i][2]=max({0ll,a[i]+dp[i-1][1],a[i],a[i]+dp[i-1][2]});
+    //deb(i)
+    //Deb(dp[i][0],dp[i][1],dp[i][2])
+    ans=max({ans,dp[i][0],dp[i][1],dp[i][2]});
+    i++;
+  }
+  deb(ans)
+}
