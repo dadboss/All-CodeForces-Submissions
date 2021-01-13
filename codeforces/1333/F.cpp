ad4c9cc10f89e6586c2d77481aea46a4b7f060b0
dadboss <shivamjha1206@gmail.com>
2021-01-14 02:18:00 +0530

    Add solution for problem `F - Kate and imperfection` Link: https://codeforces.com/contest/1333/problem/F Tags: greedy, implementation, math, number theory, sortings, two pointers, *2200 Ref: https://codeforces.com/contest/1333/submission/104227300

---
+++ codeforces/1333/F.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<unordered_map>
+#include<vector>
+#include<set>
+#include<queue>
+#include<stack>
+#include <math.h>
+#include<climits>
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define deb(x) cout<<x<<"\n";
+#define debb(x,y) cout<<x<<" "<<y<<"\n";
+#define debbb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n";
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define clt(x) 63-__builtin_clzll(x)
+#define bct(x) __builtin_popcountll(x)
+#define all(v) (v).begin(),(v).end()
+#define pi pair<ll,ll>
+#define vi vector<ll>
+#define vpi vector<pair<ll,ll>>
+#define maxq priority_queue<ll>
+#define minq priority_queue<ll,vector<ll>, greater<ll>>
+#define cont continue;
+#define reto return 0;
+#define spmod 1116295198451
+#define mod 1000000007
+#define md 998244353 
+#define N 500009
+ll a[N],f[N];
+int main()
+{
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  #ifndef ONLINE_JUDGE
+  	freopen("input.txt", "r", stdin);
+  	freopen("output.txt", "w", stdout);
+  #endif
+  ll n;
+  cin>>n;
+  ll i=2,j;
+  while(i<=n)
+  {
+  	j=i+i;
+  	while(j<=n)
+  	{
+  		a[j]=i;
+  		j+=i;
+  	}
+  	i++;
+  }
+  i=2;
+  while(i<=n)
+  {
+  	f[a[i]]++;
+  	i++;
+  }
+  while(f[0]--)
+  	cout<<1<<" ";
+  i=2;
+  while(i<=n)
+  {
+  	while(f[i]--)
+  		cout<<i<<" ";
+  	i++;
+  }
+}
