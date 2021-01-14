c3aae953fe4b12a369da780c4d1013608a73a305
dadboss <shivamjha1206@gmail.com>
2021-01-14 22:26:00 +0530

    Add solution for problem `D - Program` Link: https://codeforces.com/contest/1473/problem/D Tags: data structures, dp, implementation, strings, *1700 Ref: https://codeforces.com/contest/1473/submission/104351401

--- codeforces/1473/D.cpp
+++ codeforces/1473/D.cpp
@@ -9,16 +9,14 @@
 #include<stack>
 #include <math.h>
 #include<climits>
-//#include <ext/pb_ds/assoc_container.hpp>
-//using namespace __gnu_pbds;
 using namespace std;
 #define ll long long
 #define ff first
 #define ss second
 #define pb push_back
 #define deb(x) cout<<x<<"\n";
-#define deB(x,y) cout<<x<<" "<<y<<"\n";
-#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n";
+#define debb(x,y) cout<<x<<" "<<y<<"\n";
+#define debbb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n";
 #define YES cout<<"YES\n";
 #define Yes cout<<"Yes\n";
 #define NO cout<<"NO\n";
@@ -33,55 +31,11 @@ using namespace std;
 #define minq priority_queue<ll,vector<ll>, greater<ll>>
 #define cont continue;
 #define reto return 0;
-#define sz size()
 #define spmod 1116295198451
 #define mod 1000000007
 #define md 998244353 
 #define N 200009
-string s;
-struct node
-{
-	ll val,mn,mx;
-};
-node t[4*N];
-node merge(node a,node b)
-{
-	a.mn=min(a.mn,a.val+b.mn);
-	a.mx=max(a.mx,a.val+b.mx);
-	a.val+=b.val;
-	return a;
-}
-void build( ll v, ll tl, ll tr) {
-    if (tl == tr) {
-    	if(s[tl]=='+')
-    	{
-    		t[v].val=1;
-    		t[v].mx=1;
-    		t[v].mn=0;
-    	}
-    	else
-    	{
-    		t[v].val=-1;
-    		t[v].mx=0;
-    		t[v].mn=-1;
-    	}
-    } else {
-        ll tm = (tl + tr) / 2;
-        build(v*2, tl, tm);
-        build( v*2+1, tm+1, tr);
-        //t[v] = t[v*2] + t[v*2+1];
-        t[v]=merge(t[2*v],t[2*v+1]);
-    }
-}
-node sum(ll v, ll tl, ll tr, ll l, ll r) {
-    if (l > r) 
-        return {0,0,0};
-    if (l == tl && r == tr) {
-        return t[v];
-    }
-    ll tm = (tl + tr) / 2;
-    return merge(sum(v*2, tl, tm, l, min(r, tm)),sum(v*2+1, tm+1, tr, max(l, tm+1), r));
-}
+ll msp[N],nsp[N],mxp[N],val[N],mnp[N];
 int main()
 {
   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
@@ -89,19 +43,51 @@ int main()
   	freopen("input.txt", "r", stdin);
   	freopen("output.txt", "w", stdout);
   #endif
-  ll t=1,n,q,l,r;
+  ll t=1,n,q,l,r,i,x,num,y,z;
+  string s;
   cin>>t;
-  node rew;
   while(t--)
   {
- 	cin>>n>>q>>s;
- 	s='!'+s;
- 	build(1,1,n);
- 	while(q--)
- 	{
- 		cin>>l>>r;
- 		rew=merge(sum(1,1,n,1,l-1),sum(1,1,n,r+1,n));
- 		deb(rew.mx-rew.mn+1);
- 	}   
+  	cin>>n>>q>>s;
+  	s='!'+s;
+  	i=1;
+  	num=0;
+  	while(i<=n)
+  	{
+  		if(s[i]=='+')
+  			num++;
+  		else
+  			num--;
+  		val[i]=num;
+  		mxp[i]=max(mxp[i-1],num);
+  		mnp[i]=min(mnp[i-1],num);
+  		i++;
+  	}
+  	msp[i]=INT_MIN;
+  	nsp[i]=INT_MAX;
+  	i=n;
+  	while(i)
+  	{
+  		msp[i]=max(msp[i+1],val[i]);
+  		nsp[i]=min(nsp[i+1],val[i]);
+  		i--;
+  	}
+  	pi a[2];
+  	while(q--)
+  	{
+  		cin>>l>>r;
+  		x=val[r]-val[l-1];
+  		y=msp[r+1]-x;
+  		z=nsp[r+1]-x;
+		if(r==n)
+			y=z=0;
+		else
+		{
+			y=msp[r+1]-x;
+			z=nsp[r+1]-x;
+		}
+		//debb(y,z)
+  		deb(max(y,mxp[l-1])-min(z,mnp[l-1])+1)
+  	}
   }
 }
