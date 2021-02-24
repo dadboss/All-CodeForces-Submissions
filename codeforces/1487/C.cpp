fad38b830eaf54c1044faee82622ea2786f92460
dadboss <shivamjha1206@gmail.com>
2021-02-24 19:02:00 +0530

    Add solution for problem `C - Minimum Ties` Link: https://codeforces.com/contest/1487/problem/C Tags: brute force, constructive algorithms, dfs and similar, graphs, greedy, implementation, math, *1500 Ref: https://codeforces.com/contest/1487/submission/108405977

---
+++ codeforces/1487/C.cpp
@@ -0,0 +1,127 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<unordered_map>
+#include<vector>
+#include<set>
+#include<queue>
+#include<stack>
+#include <math.h>
+#include<climits>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n";
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define clt(x) 63-__builtin_clzll(x)
+#define bct(x) __builtin_popcountll(x)
+#define all(v) (v).begin(),(v).end()
+#define pi pair<ll,ll>
+#define vi vector<ll>
+#define vpi vector<pair<ll,ll>>
+#define maxq priority_queue<ll>
+#define minq priority_queue<ll,vector<ll>, greater<ll>>
+#define cont continue;
+#define reto return 0;
+#define sz size()
+#define spmod 1116295198451
+#define mod 1000000007
+#define md 998244353 
+#define N 200009
+//typedef tree<int,null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> //pbds;
+
+int main()
+{
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  #ifndef ONLINE_JUDGE
+  	freopen("input.txt", "r", stdin);
+  	freopen("output.txt", "w", stdout);
+  #endif
+  ll t=1,i,j,n;
+  cin>>t;
+  while(t--)
+  {
+	cin>>n;
+	if(n%2)
+	{
+		ll f[109]={};
+		i=1;
+		while(i<n)
+		{
+			j=i+1;
+			while(j<=n)
+			{
+				if(f[i]!=n/2)
+				{
+					f[i]++;
+					cout<<"1 ";
+				}
+				else
+				{
+					cout<<"-1 ";
+					f[j]++;
+				}
+				j++;
+			}
+			i++;
+		}
+	}
+	else
+	{
+		ll f[109]={},l[109]={};
+		i=1;
+		while(i<n)
+		{
+			j=i+1;
+			while(j<=n)
+			{
+				if(i+j==n+1)
+					cout<<"0 ";
+				else if(i%2)
+				{
+					if(f[i]!=n/2-1)
+					{
+						f[i]++;
+						l[j]++;
+						cout<<"1 ";
+					}
+					else
+					{
+						f[j]++;
+						l[i]++;
+						cout<<"-1 ";
+					}
+				}
+				else
+				{
+					if(l[i]!=n/2-1)
+					{
+						l[i]++;
+						f[j]++;
+						cout<<"-1 ";
+					}
+					else
+					{
+						l[j]++;
+						f[i]++;
+						cout<<"1 ";
+					}
+				}
+				j++;
+			}
+			i++;
+		}  
+	}  
+	cout<<"\n";
+  }
+}
