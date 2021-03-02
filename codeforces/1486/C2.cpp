28b36ce66723ca63de12acb00e3490d087b2b32b
dadboss <shivamjha1206@gmail.com>
2021-03-02 18:38:00 +0530

    Add solution for problem `C2 - Guessing the Greatest (hard version)` Link: https://codeforces.com/contest/1486/problem/C2 Tags: binary search, interactive, *1900 Ref: https://codeforces.com/contest/1486/submission/108878267

---
+++ codeforces/1486/C2.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<unordered_map>
+#include<vector>
+#include<set>
+#include<queue>
+#include<stack>
+#include <math.h>
+#include<climits>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n";
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define clt(x) 63-__builtin_clzll(x)
+#define bct(x) __builtin_popcountll(x)
+#define all(v) (v).begin(),(v).end()
+#define pi pair<ll,ll>
+#define vi vector<ll>
+#define vpi vector<pair<ll,ll>>
+#define maxq priority_queue<ll>
+#define minq priority_queue<ll,vector<ll>, greater<ll>>
+#define cont continue;
+#define reto return 0;
+#define sz size()
+#define spmod 1116295198451
+#define mod 1000000007
+#define md 998244353 
+#define N 200009
+//typedef tree<int,null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> //pbds;
+
+int main()
+{
+ // ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+  ll n,ans;
+  cin>>n;
+  cout<<"? ";
+  deB(1,n)
+  ll m;
+  cin>>m;
+  if(m==1)
+    goto pot;
+  cout<<"? ";
+  deB(1,m);
+  ll x;
+  cin>>x;
+  if(x==m)
+  {
+    ll st=1,e=m-1;
+    while(st<=e)
+    {
+      ll mid=(st+e)/2;
+      cout<<"? ";
+      deB(mid,m)
+      cin>>x;
+      if(x==m)
+      {
+        ans=mid;
+        st=mid+1;
+      }
+      else
+        e=mid-1;
+    }
+  }
+  else
+  {
+    pot:
+    ll st=m+1,e=n;
+    while(st<=e)
+    {
+      ll mid=(st+e)/2;
+      cout<<"? ";
+      deB(m,mid);
+      cin>>x;
+      if(x==m)
+      {
+        ans=mid;
+        e=mid-1;
+      }
+      else
+        st=mid+1;
+    }
+  }
+  cout<<"! "<<ans;
+}
