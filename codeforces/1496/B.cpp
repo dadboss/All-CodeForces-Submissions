3b9b8b4f59a56dfc63900e7e32644658d259ed91
dadboss <shivamjha1206@gmail.com>
2021-03-12 03:39:00 +0530

    Add solution for problem `B - Max and Mex` Link: https://codeforces.com/contest/1496/problem/B Tags: math, *1100 Ref: https://codeforces.com/contest/1496/submission/109739010

---
+++ codeforces/1496/B.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<unordered_map>
+#include<vector>
+#include<set>
+#include<queue>
+#include<stack>
+#include <math.h>
+#include<climits>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n";
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define clt(x) 63-__builtin_clzll(x)
+#define bct(x) __builtin_popcountll(x)
+#define all(v) (v).begin(),(v).end()
+#define pi pair<ll,ll>
+#define vi vector<ll>
+#define vpi vector<pair<ll,ll>>
+#define maxq priority_queue<ll>
+#define minq priority_queue<ll,vector<ll>, greater<ll>>
+#define cont continue;
+#define reto return 0;
+#define sz size()
+#define spmod 1116295198451
+#define mod 1000000007
+#define md 998244353 
+#define N 200009
+//typedef tree<int,null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> //pbds;
+
+int main()
+{
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+  ll t=1,n,k,i,mx,d;
+  cin>>t;
+  while(t--)
+  {
+    cin>>n>>k;
+    i=0;
+    set<ll> s;
+    set<ll> f;
+    while(i<n+10)
+    {
+      s.insert(i);
+      i++;
+    }
+    i=0;
+    mx=-1;
+    while(i<n)
+      {
+        cin>>d;
+        s.erase(d);
+        f.insert(d);
+        mx=max(mx,d);
+        i++; 
+      }   
+    //deb((mx+*s.begin()+1)/2)
+    if(*s.begin()==n)
+      deb(n+k)
+    else
+    {
+      if(k==0)
+        deb(n)
+      else if(f.find((mx+*s.begin()+1)/2)!=f.end())
+        deb(n)
+      else
+        deb(n+1)
+    }
+  }
+}
