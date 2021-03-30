25dbd02eaa9cdb72abf89237618a40e3c2cfb207
dadboss <shivamjha1206@gmail.com>
2021-03-31 03:24:00 +0530

    Add solution for problem `E - Two Houses` Link: https://codeforces.com/contest/1498/problem/E Tags: brute force, graphs, greedy, interactive, sortings, *2200 Ref: https://codeforces.com/contest/1498/submission/111511523

--- codeforces/1498/E.cpp
+++ codeforces/1498/E.cpp
@@ -42,11 +42,6 @@ using namespace std;
 pi a[N];
 int main()
 {
- // ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-  #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-  #endif
   ll n;
   cin>>n;
   ll i=1;
