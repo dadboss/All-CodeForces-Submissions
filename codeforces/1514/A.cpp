e303ab16b96780b0e3db72d6c3107a79a86272b4
dadboss <shivamjha1206@gmail.com>
2021-04-19 19:08:00 +0530

    Add solution for problem `A - Perfectly Imperfect Array` Link: https://codeforces.com/contest/1514/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1514/submission/113492413

---
+++ codeforces/1514/A.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<unordered_map>
+#include<vector>
+#include<set>
+#include<queue>
+#include<stack>
+#include <math.h>
+#include<climits>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n";
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define clt(x) 63-__builtin_clzll(x)
+#define bct(x) __builtin_popcountll(x)
+#define all(v) (v).begin(),(v).end()
+#define pi pair<ll,ll>
+#define vi vector<ll>
+#define vpi vector<pair<ll,ll>>
+#define maxq priority_queue<ll>
+#define minq priority_queue<ll,vector<ll>, greater<ll>>
+#define cont continue;
+#define reto return 0;
+#define sz size()
+#define spmod 1116295198451
+#define mod 1000000007
+#define md 998244353 
+#define N 200009
+//typedef tree<int,null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> //pbds;
+ll f[N];
+int main()
+{
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  #ifndef ONLINE_JUDGE
+  	freopen("input.txt", "r", stdin);
+  	freopen("output.txt", "w", stdout);
+  #endif
+  ll i=1;
+  while(i*i<=10000)
+  {
+  	f[i*i]=1;
+  	i++;
+  }
+  ll t=1,n,d,p;
+  cin>>t;
+  while(t--)
+  {
+ 	 cin>>n;
+ 	 p=0;
+ 	 while(n--)
+ 	 {
+ 	 	cin>>d;
+ 	 	if(f[d]==0)
+ 	 		p=1;
+ 	 }
+ 	 if(p)
+ 	 	YES
+ 	 else
+ 	 	NO
+  }
+}
