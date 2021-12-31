81b78a8511769d51ebe0eabf29f5b95cf0b5986c
dadboss <shivamjha1206@gmail.com>
2021-12-31 06:55:00 +0530

    Add solution for problem `A - IQ Test` Link: https://codeforces.com/contest/328/problem/A Tags: implementation, *1800 Ref: https://codeforces.com/contest/328/submission/141205702

---
+++ codeforces/328/A.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n";
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define clt(x) 63-__builtin_clzll(x)
+#define bct(x) __builtin_popcountll(x)
+#define all(v) (v).begin(),(v).end()
+#define pi pair<ll,ll>
+#define vi vector<ll>
+#define vpi vector<pair<ll,ll>>
+#define maxq priority_queue<ll>
+#define minq priority_queue<ll,vector<ll>, greater<ll>>
+#define cont continue;
+#define reto return 0;
+#define sz(x) (ll)(x.size()) 
+#define spmod 1116295198451
+#define md 998244353 
+#define mod 1000000007
+#define N 300009
+int main()
+{
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+  ll a,b,c,d;
+  cin>>a>>b>>c>>d;
+  if(b-a==c-b&&c-b==d-c)
+  {
+    deb(d+d-c)
+    reto
+  }
+  if(a&&b&&c&&d&&b!=a&&c!=b&&d!=c&&a*c==b*b&&d*b==c*c)
+  {
+    d*=d;
+    if(d%c)
+      deb(42)
+    else
+      deb(d/c)
+    reto
+  }
+  deb(42)
+}         
