a73af8a24187867da7469cefe1f3abc8a39dcb1c
dadboss <shivamjha1206@gmail.com>
2022-01-05 07:48:00 +0530

    Add solution for problem `C - Permutation Game` Link: https://codeforces.com/contest/1033/problem/C Tags: brute force, dp, games, *1600 Ref: https://codeforces.com/contest/1033/submission/141690043

---
+++ codeforces/1033/C.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n";
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define clt(x) 63-__builtin_clzll(x)
+#define bct(x) __builtin_popcountll(x)
+#define all(v) (v).begin(),(v).end()
+#define pi pair<ll,ll>
+#define vi vector<ll>
+#define vpi vector<pair<ll,ll>>
+#define maxq priority_queue<ll>
+#define minq priority_queue<ll,vector<ll>, greater<ll>>
+#define cont continue;
+#define reto return 0;
+#define sz(x) (ll)(x.size()) 
+#define spmod 1116295198451
+#define md 998244353 
+#define mod 1000000007
+#define N 300009
+ll dp[N],a[N],n;
+bool f(ll i)
+{
+  if(dp[i]!=-1)
+    return dp[i];
+  ll pos=i+a[i];
+  while(pos<=n)
+  {
+    if(a[pos]>a[i]&&f(pos)==0)
+      return dp[i]=1;
+    pos+=a[i];
+  }
+  pos=i-a[i];
+  while(pos>0)
+  {
+    if(a[pos]>a[i]&&f(pos)==0)
+      return dp[i]=1;
+    pos-=a[i];
+  }
+  return dp[i]=0;
+}
+int main()
+{
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+    memset(dp,-1,sizeof(dp));
+  cin>>n;
+  ll i=1;
+  while(i<=n)
+    cin>>a[i++];
+  i=1;
+  while(i<=n)
+  {
+    if(f(i))
+      cout<<'A';
+    else
+      cout<<'B';
+    i++;
+  }
+}         
