2fe187db65d66b064a430ee892dacf3a3834277d
dadboss <shivamjha1206@gmail.com>
2022-01-05 07:08:00 +0530

    Add solution for problem `D - Taxes` Link: https://codeforces.com/contest/735/problem/D Tags: math, number theory, *1600 Ref: https://codeforces.com/contest/735/submission/141688912

---
+++ codeforces/735/D.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n";
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define clt(x) 63-__builtin_clzll(x)
+#define bct(x) __builtin_popcountll(x)
+#define all(v) (v).begin(),(v).end()
+#define pi pair<ll,ll>
+#define vi vector<ll>
+#define vpi vector<pair<ll,ll>>
+#define maxq priority_queue<ll>
+#define minq priority_queue<ll,vector<ll>, greater<ll>>
+#define cont continue;
+#define reto return 0;
+#define sz(x) (ll)(x.size()) 
+#define spmod 1116295198451
+#define md 998244353 
+#define mod 1000000007
+#define N 300009
+bool f(ll n)
+{
+  ll i=2;
+  while(i*i<=n)
+  {
+    if(n%i==0)
+      return 0;
+    i++;
+  }
+  return 1;
+}
+int main()
+{
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+  ll n;
+  cin>>n;
+  if(f(n))
+  {
+    deb(1)
+    reto
+  }
+  if(n%2==0||f(n-2))
+    deb(2)
+  else
+    deb(3)
+}         
