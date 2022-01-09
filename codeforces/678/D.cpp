3720b2a9e45d8c2e66010130727e1af372b69bf5
dadboss <shivamjha1206@gmail.com>
2022-01-09 06:31:00 +0530

    Add solution for problem `D - Iterated Linear Function` Link: https://codeforces.com/contest/678/problem/D Tags: math, number theory, *1700 Ref: https://codeforces.com/contest/678/submission/142040285

---
+++ codeforces/678/D.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define deb(x) cout<<x<<"\n";
+#define deB(x,y) cout<<x<<" "<<y<<"\n";
+#define Deb(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n";
+#define YES cout<<"YES\n";
+#define Yes cout<<"Yes\n";
+#define NO cout<<"NO\n";
+#define No cout<<"No\n";
+#define clt(x) 63-__builtin_clzll(x)
+#define bct(x) __builtin_popcountll(x)
+#define all(v) (v).begin(),(v).end()
+#define pi pair<ll,ll>
+#define vi vector<ll>
+#define vpi vector<pair<ll,ll>>
+#define maxq priority_queue<ll>
+#define minq priority_queue<ll,vector<ll>, greater<ll>>
+#define cont continue;
+#define reto return 0;
+#define sz(x) (ll)(x.size()) 
+#define spmod 1116295198451
+#define md 998244353 
+#define mod 1000000007
+#define N 300009
+#define p mod
+ll power(ll x, ll y ) 
+{ 
+    ll res = 1;
+    x = x % p; 
+    while (y > 0) 
+    { 
+        if (y & 1) 
+            res = (res*x) % p; 
+        y = y>>1;  
+        x = (x*x) % p; 
+    } 
+    return res; 
+} 
+ll modInverse(ll n ) 
+{ 
+    return power(n, p-2); 
+} 
+int main()
+{
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+  ll a,b,n,x;
+  cin>>a>>b>>n>>x;
+  if(a==1)
+  {
+    deb((x+(n%mod)*b)%mod)
+    reto
+  }
+  ll ans=b*modInverse(a-1);
+  ans%=mod;
+  ans*=(power(a,n)+mod-1)%mod;
+  ans%=mod;
+  ans+=x*power(a,n);
+  ans%=mod;
+  deb(ans)
+}         
